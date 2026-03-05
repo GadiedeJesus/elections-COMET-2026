fe1df19a679dcf749bb19c91d73a69d2ca22930b
GadiedeJesus <gadiellepaulettenkongne@gmail.com>
2026-03-05 08:43:17 +0100

    Update candidats.py

--- candidats.py
+++ candidats.py
@@ -2,28 +2,28 @@ CANDIDATS = [
     {
         "id": "dean",
         "nom": "Dean Tevou",
-        "photo": "assets/photos/dean.jpg",
+        "photo": "https://imgur.com/JUSL4JQ",
         "description": "Étudiant en ISE2.",       
         "slogan": "« Agir - Servir - Impacter. Notre école mérite mieux. »"
     },
     {
         "id": "emmanuelle",
         "nom": "Emmanuelle Enangue",
-        "photo": "assets/photos/emmanuelle.jpg",
+        "photo": "https://imgur.com/4pQrV6q",
         "description": "Étudiante en ISE L2.",
         "slogan": "« Une présidente pour tous, pas pour un groupe »"
     },
     {
         "id": "achley",
         "nom": "Marie Achley Adannou",
-        "photo": "assets/photos/achley.jpg",
+        "photo": "https://imgur.com/zGu6B1N",
         "description": "Étudiante en ISE L3.",
         "slogan": "« Pour un COMET plus porteur»"
     },
     {
         "id": "sagesse",
         "nom": "Sagesse Nzambou",
-        "photo": "assets/photos/sagesse.jpg",
+        "photo": "https://imgur.com/2B89wY6",
         "description": "Étudiante en AS2.",
         "slogan": "« Professionnalisme - Innovation - Epanouissement. Avec Sagesse, des actes plus que des promesses »"
     },
@@ -35,4 +35,5 @@ CRITERES = [
     {"id": "programme",  "label": "📋 Programme"},
     {"id": "leadership", "label": "💪 Leadership"},
     {"id": "confiance",  "label": "🤝 Confiance"},
+
 ]
